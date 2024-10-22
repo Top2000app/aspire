43dd06eb26d90fe283b2bcbc75d56349bea0da35
Rick Neeft <rick.neeft@outlook.com>
2024-10-22 10:48:59 +0200

    test project

---
+++ tests/Top2000.Api.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+using HotChocolate.Execution;
+using Microsoft.Extensions.DependencyInjection;
+using Top2000.Api.GraphQL;
+using Snapshooter.MSTest;
+using HotChocolate.Execution;
+
+namespace Top2000.Api.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public async Task TestMethod1()
+    {
+        var schema = await new ServiceCollection()
+            .AddGraphQL()
+            .AddQueryType<Query>()
+            .BuildSchemaAsync();
+
+        Verify(schema.ToString());
+    }
+
+    [TestMethod]
+    public async Task LatestEdition()
+    {
+        var schema = new ServiceCollection()
+            .AddGraphQL()
+            .AddQueryType<Query>();
+        
+        var result = await schema.ExecuteRequestAsync("{ bestMovie { title } }");
+    }
+}
